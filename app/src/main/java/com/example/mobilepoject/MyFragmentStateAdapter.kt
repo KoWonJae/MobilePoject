9c69438eb4097bec5c0b7d0509767b7ef4301d24
JB <bjho606@naver.com>
2021-06-08 16:43:27 +0900

    채팅 기능 분리용 패키지 만듬
    테스트용 설명

--- app/src/main/java/com/example/mobilepoject/MyFragmentStateAdapter.kt
+++ app/src/main/java/com/example/mobilepoject/MyFragmentStateAdapter.kt
@@ -3,6 +3,7 @@ package com.example.mobilepoject
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.mobilepoject.messenger.ChatFragment
 
 class MyFragmentStateAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/mobilepoject/messenger/ChatFragment.kt
+++ app/src/main/java/com/example/mobilepoject/messenger/ChatFragment.kt
@@ -1,10 +1,11 @@
-package com.example.mobilepoject
+package com.example.mobilepoject.messenger
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.mobilepoject.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
