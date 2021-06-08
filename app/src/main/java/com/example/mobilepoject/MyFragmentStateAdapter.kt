fb09a1cdd924404afd0e5e050f194777eb361faa
Lee-Jiseung <ljss98@naver.com>
2021-06-08 15:56:26 +0900

    build basic structure

--- app/src/main/java/com/example/mobilepoject/MyFragmentStateAdapter.kt
+++ app/src/main/java/com/example/mobilepoject/MyFragmentStateAdapter.kt
@@ -1,4 +1,20 @@
 package com.example.mobilepoject
 
-class MyFragmentStateAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MyFragmentStateAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity)  {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0-> ProfileFragment()
+            1-> QueryFragment()
+            2-> ChatFragment()
+            else -> ProfileFragment()
+        }
+    }
 }
