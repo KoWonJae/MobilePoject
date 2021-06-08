27e0bef90e839f222fdc2806bba9e3d16fd2d151
KoWonJae <kwj1350@naver.com>
2021-06-08 15:35:03 +0900

    commit test wonjae

--- app/src/main/java/com/example/mobilepoject/MainActivity.kt
+++ app/src/main/java/com/example/mobilepoject/MainActivity.kt
@@ -7,7 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        init()
         ///commit test by wonjae
+    }
+
+    fun init(){
+
     }
 }
